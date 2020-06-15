2c575a87df29d3290c49d943cf082d2dffcd836d
Shivansh Yadav <shivanshyadav00@gmail.com>
2020-06-15 11:23:35 +0530

    ⚙️ Added settings page

--- sahaaya.js
+++ sahaaya.js
@@ -5,13 +5,33 @@ var _ = function(elt, command, fn) {
 	return elt.addEventListener(command, fn);
 }
 
-_($("#enableSahaaya"), "click", function() {
+var Sahaaya = function(){
+	if(localStorage.getItem("serviceStatus") == "start"){
+		Sahaaya.startService();
+	} else {
+		Sahaaya.stopService();
+	}
+}
+Sahaaya.startService = function(){
 	$(".disabled").style.display = "none";
 	$(".enabled").style.display = "block";
 	$("body").classList.add("on");
-});
-_($("#disableSahaaya"), "click", function() {
+}
+Sahaaya.stopService = function(){
+	localStorage.setItem("serviceStatus", "stop");
 	$(".disabled").style.display = "block";
 	$(".enabled").style.display = "none";
 	$("body").classList.remove("on");
+}
+
+_($("#enableSahaaya"), "click", function(){
+	Sahaaya.startService();
+});
+_($("#disableSahaaya"), "click", function(){
+	Sahaaya.stopService();
+});
+
+
+_(document, "DOMContentLoaded", function(){
+	Sahaaya();
 });
